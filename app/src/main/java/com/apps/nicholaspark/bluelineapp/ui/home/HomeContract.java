78e633c3e8b187331ed31b78f5188024764f3e5f
nicholaspark09 <nicholaspark09@gmail.com>
2016-10-21 01:25:20 -0400

    Change: Changed gradle
    Updated Ci definitions and predexing

---
+++ app/src/main/java/com/apps/nicholaspark/bluelineapp/ui/home/HomeContract.java
@@ -0,0 +1,12 @@
+package com.apps.nicholaspark.bluelineapp.ui.home;
+
+/**
+ * Created by nicholaspark on 10/21/16.
+ */
+
+public interface HomeContract {
+
+    interface Presenter{
+        void loadUser();
+    }
+}
